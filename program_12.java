085a0360dc4ea3ceb86b4ad147ac5278d6129311
omkar4771 <omkardaswadkar123@gmail.com>
2024-05-04 22:01:28 +0530

    while loop

---
+++ program_12.java
@@ -0,0 +1,19 @@
+package while_loop;
+
+public class program_12 {
+
+	public static void main(String[] args) 
+	{
+		int n=1234,n1;
+		
+		while(n>0)
+		{
+			n1=n%10;
+			n=n1/10;
+			
+			multi=n1
+		}
+
+	}
+
+}
